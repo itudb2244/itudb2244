df37e2dbfebdea46562a083ba1ae050d5b4aa1fc
Eymen Ünay <20779470+Eymay@users.noreply.github.com>
2022-12-08 14:29:30 +0300

    Invoices model added

--- models.py
+++ models.py
@@ -8,3 +8,20 @@ class Customer():
         self.DeliveryAddressLine1 = DeliveryAddressLine1
         self.DeliveryAddressLine2 = DeliveryAddressLine2
 
+class Invoices():
+    def __init__(self, CustomerID, CustomerName, PrimaryContactPersonID, PhoneNumber, WebsiteURL, DeliveryAddressLine1, DeliveryAddressLine2):
+        self.InvoiceID = InvoiceID
+        self.CustomerID = CustomerID
+        self.OrderID = CustomerName
+        self.ContactPersonID = ContactPersonID
+        self.AccountsPersonID = AccountsPersonID
+        self.InvoiceDate = InvoiceDate
+        self.CustomerPurchaseOrderNumber = CustomerPurchaseOrderNumber
+        self.DeliveryInstructions = DeliveryInstructions
+        self.InternalComments = InternalComments
+        self.DeliveryRun =DeliveryRun
+        self.RunPosition=RunPosition
+        self.ReturnedDeliveryData=ReturnedDeliveryData
+        self.ConfirmedDeliveryTime=ConfirmedDeliveryTime
+        self.ConfirmedReceivedBy=ConfirmedReceivedBy
+
