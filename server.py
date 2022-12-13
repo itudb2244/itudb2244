42fc96fb0ee30a87ba98db8c33f94986336a3345
Feyza Ozen <feyzaozen2000@gmail.com>
2022-12-14 00:26:17 +0300

    HTML Templates Added
    "Create" pages added.

--- server.py
+++ server.py
@@ -8,7 +8,30 @@ def create_app():
     app.config.from_object("settings")
 
     app.add_url_rule("/", view_func=views.home_page)
-    app.add_url_rule("/customers", view_func=views.customers_page)
+    app.add_url_rule("/customers", view_func=views.customers_page, methods=["GET","POST"])
+    app.add_url_rule("/people", view_func=views.people_page,methods=["GET","POST"])
+    app.add_url_rule("/customer-transactions", view_func=views.customer_transactions_page,methods=["GET","POST"])
+    app.add_url_rule("/invoice-lines", view_func=views.invoice_lines_page,methods=["GET","POST"])
+    app.add_url_rule("/invoices", view_func=views.invoices_page,methods=["GET","POST"])
+    app.add_url_rule("/order-lines", view_func=views.order_lines_page,methods=["GET","POST"])
+    app.add_url_rule("/orders", view_func=views.orders_page,methods=["GET","POST"])
+    app.add_url_rule("/stockitem-holdings", view_func=views.stockitem_holdings_page,methods=["GET","POST"])
+    app.add_url_rule("/stockitems", view_func=views.stockitems_page,methods=["GET","POST"])
+    app.add_url_rule("/stockitem-transactions", view_func=views.stockitem_transactions_page,methods=["GET","POST"])
+
+
+    app.add_url_rule("/people/add-people", view_func=views.add_people_page,methods=["GET","POST"])
+    app.add_url_rule("/customers/add-customer", view_func=views.add_customers_page,  methods=["GET","POST"])
+    app.add_url_rule("/customer-transactions/add-customer-transactions", view_func=views.add_customer_transactions_page,methods=["GET","POST"])
+    app.add_url_rule("/invoice-lines/add-invoice-lines", view_func=views.add_invoice_lines_page,methods=["GET","POST"])
+    app.add_url_rule("/invoices/add-invoices", view_func=views.add_invoices_page,methods=["GET","POST"])
+    app.add_url_rule("/order-lines/add-order-lines", view_func=views.add_order_lines_page,methods=["GET","POST"])
+    app.add_url_rule("/orders/add-orders", view_func=views.add_orders_page,methods=["GET","POST"])
+    app.add_url_rule("/stockitem-holdings/add-stockitem-holdings", view_func=views.add_stockitem_holdings_page,methods=["GET","POST"])
+    app.add_url_rule("/stockitems/add-stockitems", view_func=views.add_stockitems_page,methods=["GET","POST"])
+    app.add_url_rule("/stockitem-transactions/add-stockitem-transactions", view_func=views.add_stockitem_transactions_page,methods=["GET","POST"])
+
+
 
     app.config["dbname"] = "import_test.db"
 
@@ -19,7 +42,7 @@ def create_db():
         rc = call("./import_tables.sh")
 
 if __name__ == "__main__":
-    create_db() ################COMMENT to disable DB creation at every run
+  #  create_db() ################COMMENT to disable DB creation at every run
     app = create_app()
     port = app.config.get("PORT", 5000)
     app.run(port=port)
