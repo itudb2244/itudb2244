b1c06890a8cdec958b67af3658b2c91ad5faf04b
Eymen Ünay <20779470+Eymay@users.noreply.github.com>
2022-12-17 21:28:26 +0300

    kayıt

--- server.py
+++ server.py
@@ -9,30 +9,9 @@ def create_app():
 
     #CustomerTable = views.CustomerTable()
     app.add_url_rule("/", view_func=views.home_page)
-    app.add_url_rule("/customers", view_func=views.customers_page, methods=["GET","POST"])
-    app.add_url_rule("/people", view_func=views.people_page,methods=["GET","POST"])
-    app.add_url_rule("/customer-transactions", view_func=views.customer_transactions_page,methods=["GET","POST"])
-    app.add_url_rule("/invoice-lines", view_func=views.invoice_lines_page,methods=["GET","POST"])
-    app.add_url_rule("/invoices", view_func=views.invoices_page,methods=["GET","POST"])
-    app.add_url_rule("/order-lines", view_func=views.order_lines_page,methods=["GET","POST"])
-    app.add_url_rule("/orders", view_func=views.orders_page,methods=["GET","POST"])
-    app.add_url_rule("/stockitem-holdings", view_func=views.stockitem_holdings_page,methods=["GET","POST"])
-    app.add_url_rule("/stockitems", view_func=views.stockitems_page,methods=["GET","POST"])
-    app.add_url_rule("/stockitem-transactions", view_func=views.stockitem_transactions_page,methods=["GET","POST"])
-
-
-    app.add_url_rule("/people/add-people", view_func=views.add_people_page,methods=["GET","POST"])
-    app.add_url_rule("/customers/add-customer", view_func=views.add_customers_page,  methods=["GET","POST"])
-    app.add_url_rule("/customer-transactions/add-customer-transactions", view_func=views.add_customer_transactions_page,methods=["GET","POST"])
-    app.add_url_rule("/invoice-lines/add-invoice-lines", view_func=views.add_invoice_lines_page,methods=["GET","POST"])
-    app.add_url_rule("/invoices/add-invoices", view_func=views.add_invoices_page,methods=["GET","POST"])
-    app.add_url_rule("/order-lines/add-order-lines", view_func=views.add_order_lines_page,methods=["GET","POST"])
-    app.add_url_rule("/orders/add-orders", view_func=views.add_orders_page,methods=["GET","POST"])
-    app.add_url_rule("/stockitem-holdings/add-stockitem-holdings", view_func=views.add_stockitem_holdings_page,methods=["GET","POST"])
-    app.add_url_rule("/stockitems/add-stockitems", view_func=views.add_stockitems_page,methods=["GET","POST"])
-    app.add_url_rule("/stockitem-transactions/add-stockitem-transactions", view_func=views.add_stockitem_transactions_page,methods=["GET","POST"])
-
-
+    app.add_url_rule("/customers", endpoint="customers_page", view_func=views.CustomerTable().get_table, methods=["GET","POST"])
+    app.add_url_rule("/people", view_func=views.PeopleTable().get_table, methods=["GET","POST"])
+    app.add_url_rule("/customers/add-customer", view_func=views.CustomerTable().add_row,  methods=["GET","POST"])
     app.add_url_rule("/customer/<int:id>", view_func=views.get_customer, methods=["GET", "POST"])
 
     app.config["dbname"] = "import_test.db"
