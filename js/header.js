aa058f28b3401b8fc560b477187a0ace1486322c
sulebeyza <sulebeyzakaradag@gmail.com>
2022-12-18 12:17:49 +0300

    js file updated

--- js/header.js
+++ js/header.js
@@ -13,15 +13,3 @@ navToggle.addEventListener("click", function () {
     searchForm.classList.remove("active");
   }
 });
-
-let searchToggle = document.querySelector(".search__toggle");
-let searchForm = document.querySelector(".search__form");
-
-searchToggle.addEventListener("click", showSearch);
-
-function showSearch() {
-  searchForm.classList.toggle("active");
-  navToggle.setAttribute("aria-expanded", "false");
-  navToggle.setAttribute("aria-label", "menu");
-  navWrapper.classList.remove("active");
-}
